585d4ba5e9c3bdcd29ed2bd30f831b7d0f1b05b9
Pavel <pavel.agejkin@gmail.com>
2019-07-10 14:47:10 +0300

    final Test

--- Tests.cs
+++ Tests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.IO;
+using System.Linq;
 using FluentAssertions;
 using Xunit;
 
@@ -72,13 +73,44 @@ namespace MobilePayHomework
 2018-09-04 NETTO    200
 2018-10-22 7-ELEVEN 300
 2018-10-29 7-ELEVEN 150
-");
+", true);
            sut.Output.Select(x => x.Value).Should().BeEquivalentTo(new[]
            {
                new Fee("2018-09-02","7-ELEVEN", 30.20m),
                new Fee("2018-09-04","NETTO"   , 31.00m),
                new Fee("2018-10-22","7-ELEVEN", 32.00m),
                new Fee("2018-10-29","7-ELEVEN", 1.50m),
+           });
+        }
+        [Fact]
+        public void UserStory6_discount_for_telia()
+        {
+            var lines = File.ReadAllText("./transactions.txt");
+           var sut =  new InvoicingPipeline(lines, true);
+           sut.Output.Select(x => x.Value).Should().BeEquivalentTo(new[]
+           {
+               new Fee("2018-09-01","7-ELEVEN", 30.00m),
+               new Fee("2018-09-04","CIRCLE_K", 29.80m),
+               new Fee("2018-09-07","TELIA", 29.90m),
+               new Fee("2018-09-09","NETTO", 30.00m),
+               new Fee("2018-09-13","CIRCLE_K", 0.80m ),
+               new Fee("2018-09-16","TELIA", 0.90m ),
+               new Fee("2018-09-19","7-ELEVEN", 1.00m ),
+               new Fee("2018-09-22","CIRCLE_K", 0.80m ),
+               new Fee("2018-09-25","TELIA", 0.90m ),
+               new Fee("2018-09-28","7-ELEVEN", 1.00m ),
+               new Fee("2018-09-30","CIRCLE_K", 0.80m ),
+               new Fee("2018-10-01","7-ELEVEN", 30.00m),
+               new Fee("2018-10-04","CIRCLE_K", 29.80m),
+               new Fee("2018-10-07","TELIA", 29.90m),
+               new Fee("2018-10-10","NETTO", 30.00m),
+               new Fee("2018-10-13","CIRCLE_K", 0.80m ),
+               new Fee("2018-10-16","TELIA", 0.90m ),
+               new Fee("2018-10-19","7-ELEVEN", 1.00m ),
+               new Fee("2018-10-22","CIRCLE_K", 0.80m ),
+               new Fee("2018-10-25","TELIA", 0.90m ),
+               new Fee("2018-10-28","7-ELEVEN", 1.00m ),
+               new Fee("2018-10-30","CIRCLE_K", 0.80m ),
            });
         }
     }
